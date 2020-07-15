6f5bf68dad7d4cbdde9860799942d27f6587c3f8
yuko yuri <yuriko11d@gmail.com>
2020-07-15 20:54:06 +0900

    carc-bmi

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,38 @@ import React, { FC, useState } from 'react';
 import './App.css';
 
 const App: FC = () => {
-  const [value, setValue] = useState('');
+  const [height, setHeight] = useState('');
+  const [weight, setWeight] = useState('');
+  const [bmi, setBmi] = useState('');
 
-  const displayValue = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setValue(e.target.value);
+  const displayHeight = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setHeight(e.target.value);
+  };
+
+  const displayWeight = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setWeight(e.target.value);
+  };
+
+  const calcBmi = () => {
+    const result =
+      Math.round((Number(weight) / (Number(height) / 100) ** 2) * 10) / 10;
+    setBmi(String(result));
   };
 
   return (
     <div className="container">
       <div className="input">
-        <input type="text" onChange={displayValue} />
+        <input type="text" onChange={displayHeight} />
       </div>
-      <div className="display">{value}</div>
+      <div className="input">
+        <input type="text" onChange={displayWeight} />
+      </div>
+      <div className="button">
+        <button type="button" name="calc-bmi" onClick={calcBmi}>
+          計算
+        </button>
+      </div>
+      <div className="display">BMI:{bmi}</div>
     </div>
   );
 };
