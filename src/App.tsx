809927d2070b0682a8ce3b046d316cd4a56d2d2c
Yuko Yuri <y.yuri@everyleaf.com>
2020-07-16 18:38:14 +0900

    初期値変更

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,20 @@ import React, { FC, useState } from 'react';
 import './App.css';
 
 const App: FC = () => {
-  const [height, setHeight] = useState('');
-  const [weight, setWeight] = useState('');
+  const [height, setHeight] = useState(0);
+  const [weight, setWeight] = useState(0);
   const [bmi, setBmi] = useState('');
 
   const displayHeight = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setHeight(e.target.value);
+    setHeight(Number(e.target.value));
   };
 
   const displayWeight = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setWeight(e.target.value);
+    setWeight(Number(e.target.value));
   };
 
   const calcBmi = () => {
-    const result =
-      Math.round((Number(weight) / (Number(height) / 100) ** 2) * 10) / 10;
+    const result = Math.round((weight / (height / 100) ** 2) * 10) / 10;
     setBmi(String(result));
   };
 
