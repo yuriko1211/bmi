1707b24bec9f9411e10f53df1183b8b36431cf05
yuko yuri <yuriko11d@gmail.com>
2020-08-05 21:12:06 +0900

    ボタンをなくした

--- src/App.tsx
+++ src/App.tsx
@@ -6,17 +6,19 @@ const App: FC = () => {
   const [weight, setWeight] = useState(0);
   const [bmi, setBmi] = useState('');
 
+  const calcurateBmi = () => {
+    const result = Math.round((weight / (height / 100) ** 2) * 10) / 10;
+    setBmi(String(result));
+  };
+
   const displayHeight = (e: React.ChangeEvent<HTMLInputElement>) => {
     setHeight(Number(e.target.value));
+    calcurateBmi();
   };
 
   const displayWeight = (e: React.ChangeEvent<HTMLInputElement>) => {
     setWeight(Number(e.target.value));
-  };
-
-  const calcBmi = () => {
-    const result = Math.round((weight / (height / 100) ** 2) * 10) / 10;
-    setBmi(String(result));
+    calcurateBmi();
   };
 
   return (
@@ -34,16 +36,6 @@ const App: FC = () => {
             体重
             <input type="text" id="weight" onChange={displayWeight} />
           </label>
-        </div>
-        <div className="button">
-          <button
-            className="ui button"
-            type="button"
-            name="calc-bmi"
-            onClick={calcBmi}
-          >
-            計算
-          </button>
         </div>
       </form>
       <div className="display">BMI:{bmi}</div>
